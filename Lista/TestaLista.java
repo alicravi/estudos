57fbbc146a8026af6db907c3afe4855cc9deddb4
alinecraviee <rizahawk@gmail.com>
2015-10-19 22:19:31 -0300

    Lista Simplesmente Encadeada - JAVA

---
+++ Lista/TestaLista.java
@@ -0,0 +1,12 @@
+
+public class TestaLista{
+	
+	public static void main (String[] args){
+		ListaContato lc = new ListaContato();
+		lc.iniciar();
+		lc.inserirNoInicio("Jô",9999);
+		lc.inserirNoInicio("Ana",3333);
+		lc.inserirNoInicio("Zé",1111);
+
+	}
+}
